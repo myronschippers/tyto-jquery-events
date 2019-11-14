8c9d3cc4c6defec6dce22792980b14c6848f72df
Myron <dubtwee@gmail.com>
2019-11-14 10:44:03 -0600

    adding the delete opf an item

--- scripts/client.js
+++ scripts/client.js
@@ -8,6 +8,7 @@ function onReady() {
     // do something when DOM is ready
 
     $('.js-addPerson').on('click', onAddPerson);
+    $('.js-people').on('click', '.js-person', onClickDeletePerson)
 }
 
 function onAddPerson(event) {
@@ -28,9 +29,23 @@ function onAddPerson(event) {
     render();
 
     // CLEARs FORM FIELDS
-    $('.js-field-firstname').val('');
-    $('.js-field-lastname').val('');
-    $('.js-field-age').val('');
+    // $('.js-field-firstname').val('');
+    // $('.js-field-lastname').val('');
+    // $('.js-field-age').val('');
+}
+
+function onClickDeletePerson(event) {
+    console.log(this);
+    const $clickedElem = $(this);
+    console.log($clickedElem);
+    const indexOfItem = $clickedElem.data('index');
+    console.log(indexOfItem);
+    // Remove person from list
+    PEOPLE_LIST.splice(indexOfItem, 1);
+    console.log(PEOPLE_LIST);
+
+    // re-render list of people
+    render();
 }
 
 function render() {
@@ -38,10 +53,12 @@ function render() {
 
     $list.empty();
 
-    for (let person of PEOPLE_LIST) {
+    for (let index in PEOPLE_LIST) {
+        const person = PEOPLE_LIST[index]
         $list.append(`
-            <li>
+            <li data-kittykat="meow">
                 ${person.fullName} is ${person.age} years old.
+                <button class="js-person" data-index="${index}">Delete</button>
             </li>
         `);
     }
