0cb95214fe6ab7a4cf220fa87dd5f19e90f58e3a
Myron <dubtwee@gmail.com>
2019-11-14 10:06:29 -0600

    adding new people to the array

--- scripts/client.js
+++ scripts/client.js
@@ -1,5 +1,7 @@
 console.log('Hello Tyto!!!');
 
+const PEOPLE_LIST = [];
+
 $(document).ready(onReady);
 
 function onReady() {
@@ -14,8 +16,19 @@ function onAddPerson(event) {
     const age = $('.js-field-age').val();
 
     console.log(firstName, lastName, age);
+    // add person info to list
+    const newPerson = {
+        firstName: firstName,
+        lastName: lastName,
+        fullName: firstName + ' ' + lastName,
+        age: parseInt(age),
+    };
+    PEOPLE_LIST.push(newPerson);
+    console.log(PEOPLE_LIST);
 
+    // CLEARs FORM FIELDS
     $('.js-field-firstname').val('');
     $('.js-field-lastname').val('');
     $('.js-field-age').val('');
 }
+
