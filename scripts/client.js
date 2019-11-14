342ed7b69462b7f3125d687900e841114d3a6e1b
Myron <dubtwee@gmail.com>
2019-11-14 10:15:51 -0600

    render list of people to DOM

--- scripts/client.js
+++ scripts/client.js
@@ -25,10 +25,26 @@ function onAddPerson(event) {
     };
     PEOPLE_LIST.push(newPerson);
     console.log(PEOPLE_LIST);
+    render();
 
     // CLEARs FORM FIELDS
     $('.js-field-firstname').val('');
     $('.js-field-lastname').val('');
     $('.js-field-age').val('');
+}
+
+function render() {
+    const $list = $('.js-people');
+
+    $list.empty();
+
+    for (let person of PEOPLE_LIST) {
+        $list.append(`
+            <li>
+                ${person.fullName} is ${person.age} years old.
+            </li>
+        `);
+    }
+
 }
 
