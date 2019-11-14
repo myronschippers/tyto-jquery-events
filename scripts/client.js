6bebf9d1f900f0187919a2c825664e90a5a4658f
Myron <dubtwee@gmail.com>
2019-11-14 09:56:19 -0600

    adding event handler for form submit

--- scripts/client.js
+++ scripts/client.js
@@ -4,4 +4,18 @@ $(document).ready(onReady);
 
 function onReady() {
     // do something when DOM is ready
+
+    $('.js-addPerson').on('click', onAddPerson);
+}
+
+function onAddPerson(event) {
+    const firstName = $('.js-field-firstname').val();
+    const lastName = $('.js-field-lastname').val();
+    const age = $('.js-field-age').val();
+
+    console.log(firstName, lastName, age);
+
+    $('.js-field-firstname').val('');
+    $('.js-field-lastname').val('');
+    $('.js-field-age').val('');
 }
